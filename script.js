93267302b44ee6828f48092e8968bd47dd86ddda
Radek <radoslawef@gmail.com>
2022-11-14 17:16:59 +0100

    added API fetch for funcionals

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+
+const API_KEY = '&appid=9d309636ca1425c2488b0af0728f7a2b'
+const API_UNITS = '&units=metric'
+
+const getWeather = () => {
+    const city = input.value || 'London'
+    const URL = API_LINK + city + API_KEY + API_UNITS
+
+    axios.get(URL).then(res => {
+        console.log(res.data);
+        const temp = res.data.main.temp
+        const hum = res.data.main.humidity
+        const weather = res.data.weather.main
+
+        cityName.textContent = res.data.name
+        temperature.textContent = Math.floor(temp) + '°C'
+        humidity.textContent = hum + '%'
+    })
+}
+
+
+
+
+getWeather()
