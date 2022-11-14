7f84165c1126e52acb8eb2fd34ad92ff043c8a33
Radek <radoslawef@gmail.com>
2022-11-14 19:21:33 +0100

    added multiple weather check status/finished

--- script.js
+++ script.js
@@ -13,22 +13,48 @@ const API_KEY = '&appid=9d309636ca1425c2488b0af0728f7a2b'
 const API_UNITS = '&units=metric'
 
 const getWeather = () => {
-    const city = input.value || 'London'
-    const URL = API_LINK + city + API_KEY + API_UNITS
+	const city = input.value || 'London'
+	const URL = API_LINK + city + API_KEY + API_UNITS
 
-    axios.get(URL).then(res => {
-        console.log(res.data);
-        const temp = res.data.main.temp
-        const hum = res.data.main.humidity
-        const weather = res.data.weather.main
+	axios
+		.get(URL)
+		.then(res => {
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			const status = Object.assign({}, ...res.data.weather)
+			// res.data.weather[0]
 
-        cityName.textContent = res.data.name
-        temperature.textContent = Math.floor(temp) + '°C'
-        humidity.textContent = hum + '%'
-    })
+			warning.textContent = ''
+			input.value = ''
+
+			if (status.id.toString().charAt(0) == '2') {
+				photo.setAttribute('src', './img/thunderstorm.png')
+			} else if (status.id.toString().charAt(0) == '3') {
+				photo.setAttribute('src', './img/drizzle.png')
+			} else if (status.id.toString().charAt(0) == '5') {
+				photo.setAttribute('src', './img/rain.png')
+			} else if (status.id.toString().charAt(0) == '6') {
+				photo.setAttribute('src', './img/ice.png')
+			} else if (status.id.toString().charAt(0) == '7') {
+				photo.setAttribute('src', './img/fog.png')
+			} else if (status.id.toString().charAt(0) == '8' && status.id !== 800) {
+				photo.setAttribute('src', './img/cloud.png')
+			} else if (status.id === 800) {
+				photo.setAttribute('src', './img/sun.png')
+			}
+
+			weather.textContent = status.main
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.floor(temp) + '°C'
+			humidity.textContent = hum + '%'
+		})
+		.catch(() => (warning.textContent = 'Wpisz poprawną nazwę miasta!'))
+}
+const enterGetWeather = e => {
+	if (e.key === 'Enter') {
+		getWeather()
+	}
 }
 
-
-
-
-getWeather()
+input.addEventListener('keyup', enterGetWeather)
+button.addEventListener('click', getWeather)
